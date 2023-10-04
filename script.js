266468e68c9b3bd68d4c9d25fbc60ec7925d2b24
Dhruv Arora <dhruvarora.code@gmail.com>
2023-10-04 22:58:01 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+console.log("hello world");
+
+const restaurant = {
+  name: "Classico Italino",
+  location: "Via Angelo Tavanti 23, Firenze, Italy",
+  categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
+  starterMenu: ["Focaccia", "Bruschetta", "Garilic Bread", "Caprese Salad"],
+  mainMenu: ["Pizza", "Pasta", "Risotto"],
+
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0,
+      close: 24,
+    },
+  },
+
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu];
+  },
+};
+
+const arr = [2, 3, 4];
+const [x, y, z] = arr;
+console.log(x, y, z);
+
+const 
