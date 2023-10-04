e7aa364cdd0d902b01bb61842dacc1df0b41898c
Dhruv Arora <dhruvarora.code@gmail.com>
2023-10-04 23:54:50 +0530

    implemented short circuiting

--- script.js
+++ script.js
@@ -49,36 +49,5 @@ const restaurant = {
   },
 };
 
-const arr = [1, 2, ...[3, 4]];
-console.log(arr);
+console.log(3 || "Jonas");
 
-const [a, b, ...others] = [1, 2, 3, 4, 5];
-console.log(a, b, others);
-
-const [pizza, , risotto, ...otherFood] = [
-  ...restaurant.mainMenu,
-  ...restaurant.starterMenu,
-];
-console.log(pizza, risotto, otherFood);
-
-const { sat, ...weekDays } = restaurant.openingHours;
-console.log(sat, weekDays);
-
-const add = function (...numbers) {
-  //   console.log(numbers);
-  let sum = 0;
-  for (let i = 0; i < numbers.length; i++) {
-    sum += numbers[i];
-  }
-  console.log(sum);
-};
-
-add(2, 3);
-add(5, 3, 7, 2);
-add(8, 2, 5, 3, 2, 1, 4);
-
-const x = [23, 5, 7];
-add(...x);
-
-restaurant.orderPizza("mushroom", "onion", "olives", "spinach");
-restaurant.orderPizza("mushroom");
