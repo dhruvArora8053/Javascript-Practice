fd0cf66d98ae1a1931676e2cee09e5796f8a6ec9
Dhruv Arora <dhruvarora.code@gmail.com>
2023-10-12 22:05:28 +0530

    string methods implemented

--- script.js
+++ script.js
@@ -1,58 +1,51 @@
 "use strict";
+//Left:-
+//1. String method Practice
 
-const airline = "TAP Air Portugal";
+console.log("a+very+nice+string".split("+"));
+console.log("Jonas Schmedtmann".split(" "));
 
-console.log(airline.toLowerCase());
-console.log(airline.toUpperCase());
-console.log("jonas".toUpperCase());
+const [firstName, lastName] = "Jonas Schmedtmann".split(" ");
+console.log(firstName, lastName);
 
-const passenger = "jOnAs";
-const passengerLower = passenger.toLowerCase();
+const newName = ["Mr.", firstName, lastName.toUpperCase()].join(" ");
+console.log(newName);
 
-const passengerCorrect =
-  passengerLower[0].toUpperCase() + passengerLower.slice(1);
-console.log(passengerCorrect);
+const capitalizeName = function (name) {
+  const names = name.split(" ");
+  let arr = [];
 
-const email = "hello@jonas.io";
-const loginEmail = "   Hello@Jonas.Io \n";
-
-const lowerEmail = loginEmail.toLowerCase();
-const trimmedEmail = lowerEmail.trim();
-console.log(trimmedEmail);
-
-const normalisedEmail = loginEmail.toLowerCase().trim();
-console.log(normalisedEmail);
-console.log(email === normalisedEmail);
-
-const priceGB = "288,96€";
-const priceUS = priceGB.replace("€", "$").replace(",", ".");
-console.log(priceUS);
-
-const announcement =
-  "All passengers come to boarding door 23. Boarding door 23!";
-
-console.log(announcement.replace("door", "gate"));
-console.log(announcement.replaceAll("door", "gate"));
-
-const plane = "Airbus A320neo";
-
-console.log(plane.includes("A320"));
-console.log(plane.includes("Boeing"));
-console.log(plane.includes("Air"));
-console.log(plane.includes("Airb"));
-
-if (plane.startsWith("Airbus") && plane.endsWith("neo")) {
-  console.log("Part of the new Airbus family");
-}
-
-const checkBaggage = function (items) {
-  const baggage = items.toLowerCase();
-
-  if (baggage.includes("knife") || baggage.includes("gun")) {
-    console.log("You are not allowed on board");
-  } else console.log("Welcome Aboard");
+  for (const n of names) {
+    // arr += n[0].toUpperCase() + n.slice(1) + " ";
+    // arr.push(n[0].toUpperCase() + n.slice(1));
+    arr.push(n.replace(n[0], n[0].toUpperCase()));
+  }
+  console.log(arr.join(" "));
 };
 
-checkBaggage("I have a laptop, some Food and a pocked Knife");
-checkBaggage("Socks and Camera");
-checkBaggage("Got some snacks and a gun for protection");
+capitalizeName("jeesica ann smith davis");
+capitalizeName("jonas schmedtmann");
+
+const message = "Go to gate 23!";
+console.log(message.padStart(25, "+"));
+console.log("Jonas".padStart(25, "+"));
+console.log(message.padStart(25, "+").padEnd(35, "+"));
+
+const maskCreditCard = function (number) {
+  const str = number + "";
+  console.log(str.slice(-4).padStart(str.length, "*"));
+};
+
+maskCreditCard(5923793257923579);
+maskCreditCard("2357953279357293572");
+
+const message2 = "Bad weather... All Departures Delayed... ";
+console.log(message2.repeat(5));
+
+const planesInLine = function (n) {
+  console.log(`Their are ${n} planes in line ${"✈️".repeat(n)}`);
+};
+
+planesInLine(5);
+planesInLine(3);
+planesInLine(10);
