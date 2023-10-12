19064f86a0a93b888b3f63eaff96b16529d634f6
Dhruv Arora <dhruvarora.code@gmail.com>
2023-10-12 20:26:57 +0530

    maps iteration implemented

--- script.js
+++ script.js
@@ -53,41 +53,30 @@ const restaurant = {
   },
 };
 
-const rest = new Map();
-rest.set("name", "Classico Italiano");
-rest.set(1, "Firenze Italy");
-rest.set(2, "Lisbon, Portugal");
-console.log(rest.set(2, "Lisbon, Portugal"));
+const question = new Map([
+  ["question", "What is the best programming language in the world"],
+  [1, "C"],
+  [2, "Java"],
+  [3, "Javascript"],
+  ["correct", 3],
+  [true, "Correct"],
+  [false, "Try Again"],
+]);
+console.log(question);
+console.log(Object.entries(openingHours));
 
-rest
-  .set("categories", ["Italian", "Pizzeria", "Vegetarian", "Organic"])
-  .set("open", 11)
-  .set("close", 23)
-  .set(true, "We are open")
-  .set(false, "We are closed");
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap);
 
-console.log(rest);
+console.log(question.get("question"));
 
-console.log(rest.get("name"));
-console.log(rest.get(true));
-console.log(rest.get("true"));
-console.log(rest.get(1));
+for (const [key, value] of question) {
+  if (typeof key === "number") {
+    console.log(`Answer ${key}: ${value}`);
+  }
+}
 
-const time = 21;
-console.log(rest.get(time > rest.get("open") && time < rest.get("close")));
-console.log(rest.has("categories"));
-rest.delete(2);
-console.log(rest);
-console.log(rest.size);
-
-// rest.clear();
-// console.log(rest);
-
-rest.set([1, 2], "Test");
-console.log(rest);
-
-const arr = [1, 2];
-rest.set(arr, "Test");
-console.log(rest.get(arr));
-
-rest.set(document.querySelector("h1"), "Heading");
+console.log([...question]);
+console.log(question.entries());
+console.log(question.keys());
+console.log(question.values());
