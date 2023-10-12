619f295a52676d7bfd6f7ef426685de0d1906d91
Dhruv Arora <dhruvarora.code@gmail.com>
2023-10-12 20:12:52 +0530

    maps implemented

--- script.js
+++ script.js
@@ -53,38 +53,41 @@ const restaurant = {
   },
 };
 
-const orderSet = new Set([
-  "Pasta",
-  "Pizza",
-  "Pizza",
-  "Risotto",
-  "Pasta",
-  "Pizza",
-]);
+const rest = new Map();
+rest.set("name", "Classico Italiano");
+rest.set(1, "Firenze Italy");
+rest.set(2, "Lisbon, Portugal");
+console.log(rest.set(2, "Lisbon, Portugal"));
 
-console.log(orderSet);
-console.log(new Set("Jonass"));
-console.log(orderSet.size);
-console.log(orderSet.has("Pizza"));
-console.log(orderSet.has("Bread"));
+rest
+  .set("categories", ["Italian", "Pizzeria", "Vegetarian", "Organic"])
+  .set("open", 11)
+  .set("close", 23)
+  .set(true, "We are open")
+  .set(false, "We are closed");
 
-orderSet.add("Garlic Bread");
-orderSet.add("Garlic Bread");
-orderSet.delete("Risotto");
-console.log(orderSet);
+console.log(rest);
 
-for (const order of orderSet) console.log(order);
+console.log(rest.get("name"));
+console.log(rest.get(true));
+console.log(rest.get("true"));
+console.log(rest.get(1));
 
-const staff = ["Waiter", "Chef", "Waiter", "Manager", "Chef", "Waiter"];
+const time = 21;
+console.log(rest.get(time > rest.get("open") && time < rest.get("close")));
+console.log(rest.has("categories"));
+rest.delete(2);
+console.log(rest);
+console.log(rest.size);
 
-const staffUnique = new Set(staff);
-console.log(staffUnique);
+// rest.clear();
+// console.log(rest);
 
-const staffUniqueArr = [...new Set(staff)];
-console.log(staffUniqueArr);
+rest.set([1, 2], "Test");
+console.log(rest);
 
-console.log(
-  new Set(["Waiter", "Chef", "Waiter", "Manager", "Chef", "Waiter"]).size
-);
+const arr = [1, 2];
+rest.set(arr, "Test");
+console.log(rest.get(arr));
 
-console.log(new Set("jonasschmedtmann").size);
+rest.set(document.querySelector("h1"), "Heading");
