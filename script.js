c4101bb572a0a5e12ebc3d8145feabe5dd57265b
Dhruv Arora <dhruvarora.code@gmail.com>
2023-10-12 14:28:52 +0530

    scoping implemented

---
+++ script.js
@@ -0,0 +1,37 @@
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+  console.log(firstName);
+  return age;
+}
+const firstName = "Jonas";
+calcAge(1991);
+
+function calcAge1(birthYear) {
+  const age = 2037 - birthYear;
+
+  function printAge() {
+    let output = `${firstName1}, You are ${age}, born in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      var millenial = true;
+      const firstName1 = "Steven";
+
+      const str = `Oh, and you're a millenial, ${firstName1}`;
+      console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+
+      const output = "New output";
+    }
+    // console.log(str);
+    console.log(millenial);
+  }
+
+  printAge();
+  return age;
+}
+const firstName1 = "Jonas";
+calcAge1(1991);
