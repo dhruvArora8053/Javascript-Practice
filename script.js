ac2f73e2b9209ee75a80e6612a179da26ea3a82f
Dhruv Arora <dhruvarora.code@gmail.com>
2023-10-12 15:15:22 +0530

    regular vs arrow function implemented

--- script.js
+++ script.js
@@ -1,37 +1,80 @@
 "use strict";
 
-console.log(this);
-
-const calcAge = function (birthYear) {
-  console.log(2037 - birthYear);
-  console.log(this);
-};
-calcAge(1991);
-
-const calcAgeArrow = (birthYear) => {
-  console.log(2037 - birthYear);
-  console.log(this);
-};
-calcAgeArrow(1980);
-
 const jonas = {
+  firstName: "Jonas",
   year: 1991,
   calcAge: function () {
     console.log(this);
     console.log(2037 - this.year);
   },
+
+  greet: () => console.log(`Hey ${this.firstName}`),
+};
+jonas.greet();
+console.log(this.firstName);
+
+var firstName = "Matilda";
+
+const jonas1 = {
+  firstName: "Jonas",
+  year: 1991,
+  calcAge: function () {
+    console.log(this);
+    console.log(2037 - this.year);
+  },
+
+  greet: () => {
+    console.log(this);
+    console.log(this.firstName);
+    console.log(`Hey ${this.firstName}`);
+  },
+};
+jonas1.greet();
+
+const jonas2 = {
+  firstName: "Jonas",
+  year: 1991,
+  calcAge: function () {
+    console.log(this);
+    console.log(2037 - this.year);
+
+    // const self = this;
+    // const isMillenial = function () {
+    //   console.log(this);
+    //   console.log(self);
+    // //   console.log(this.year >= 1981 && this.year <= 1996);
+    //   console.log(self.year >= 1981 && self.year <= 1996);
+    // };
+
+    const isMillenial = () => {
+      console.log(this);
+      console.log(self.year >= 1981 && self.year <= 1996);
+    };
+
+    isMillenial();
+  },
+
+  greet: () => {
+    console.log(this);
+    console.log(this.firstName);
+    console.log(`Hey ${this.firstName}`);
+  },
 };
 
-// jonas.calcAge();
+jonas2.calcAge();
+jonas2.greet();
 
-const matilda = {
-  year: 2017,
+const addExpr = function (a, b) {
+  console.log(arguments);
+  return a + b;
 };
 
-matilda.calcAge = jonas.calcAge;
-console.log(matilda);
-matilda.calcAge();
+addExpr(2, 5);
+addExpr(2, 5, 8, 5);
 
-const f = jonas.calcAge;
-console.log(f);
-f();
+const addArrow = (a, b) => {
+  console.log(arguments);
+  return a + b;
+};
+
+addArrow(2, 5, 8);
