1c912300ef5cd30bf652115f528cbddf11c7699c
Dhruv Arora <dhruvarora.code@gmail.com>
2023-10-12 23:51:47 +0530

    reduce method implemented

--- script.js
+++ script.js
@@ -4,3 +4,8 @@
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
+const globalBalance = movements.reduce((acc, mov, i, arr) => acc + mov, 0);
+console.log(globalBalance);
+
+const max = movements.reduce((acc, mov, i, arr) => (acc < mov ? mov : acc), 0);
+console.log(max);
