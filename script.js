60d0e1692912dd706d67c1d94eee9ac2542d4513
Dhruv Arora <dhruvarora.code@gmail.com>
2023-10-12 23:59:16 +0530

    find method implemented

--- script.js
+++ script.js
@@ -3,9 +3,7 @@
 //1. String method Practice
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+console.log(movements);
 
-const totalDeposited = movements
-  .filter((mov) => mov > 0)
-  .map((mov) => mov * 1.1)
-  .reduce((acc, mov) => acc + mov, 0);
-console.log(totalDeposited);
+const firstWithdrawal = movements.find((mov) => mov < 0);
+console.log(firstWithdrawal);
