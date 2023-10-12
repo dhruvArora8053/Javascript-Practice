96c9a8a1b3e8ed21744046bc69caea062a12e846
Dhruv Arora <dhruvarora.code@gmail.com>
2023-10-12 23:46:46 +0530

    filter implemented

--- script.js
+++ script.js
@@ -4,15 +4,3 @@
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
-const eurToUsd = 1.1;
-const movementsUSD = movements.map((mov) => mov * eurToUsd);
-console.log(movements);
-console.log(movementsUSD);
-
-const movementsDescirption = movements.map(
-  (mov, i, arr) =>
-    `Movement ${i + 1}: You ${mov > 0 ? "deposited" : "withdrew"} ${Math.abs(
-      mov
-    )}`
-);
-console.log(movementsDescirption);
