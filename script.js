f947397f1347ee7855ac2dc277d62b96619d9083
Dhruv Arora <dhruvarora.code@gmail.com>
2023-10-13 12:35:26 +0530

    implemented timers

--- script.js
+++ script.js
@@ -2,22 +2,25 @@
 //Left:-
 //1. String method Practice
 
-const num = 3884764.23;
-console.log("US:   ", new Intl.NumberFormat("en-US").format(num));
-console.log("Germany:   ", new Intl.NumberFormat("de-DE").format(num));
-console.log("Syria:   ", new Intl.NumberFormat("ar-SY").format(num));
+const ingredients = ["spinach", "olive"];
 
-console.log(
-  navigator.language,
-  new Intl.NumberFormat(navigator.language).format(num)
+const pizzaTimer = setTimeout(
+  (ing1, ing2) => console.log(`Here is your pizza with ${ing1} and ${ing2} 🍕`),
+  2000,
+  ...ingredients
 );
 
-const options = {
-  style: "currency",
-  unit: "mile-per-hour",
-  currency: "EUR",
-};
+if (ingredients.includes("spinach")) clearTimeout(pizzaTimer);
 
-console.log("US:   ", new Intl.NumberFormat("en-US", options).format(num));
-console.log("Germany:   ", new Intl.NumberFormat("de-DE", options).format(num));
-console.log("Syria:   ", new Intl.NumberFormat("ar-SY", options).format(num));
+setInterval(() => {
+  console.log(new Date());
+}, 1000);
+
+setInterval(() => {
+    let date = new Date();
+    let hour = date.getHours();
+    let minute = date.getMinutes();
+    let sec = date.getSeconds();
+    console.log(`${hour}:${minute}:${sec}`);
+  }, 1000);
+  
