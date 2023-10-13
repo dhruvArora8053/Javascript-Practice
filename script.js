0d0c919944db16f72facf12d88d0c327215bfc1f
Dhruv Arora <dhruvarora.code@gmail.com>
2023-10-13 20:24:47 +0530

    implemented call method

--- script.js
+++ script.js
@@ -3,15 +3,38 @@
 //1. String method Practice
 //2. setters and getters
 
-const greet = function (greeting) {
-  return function (name) {
-    console.log(`${greeting} ${name}`);
-  };
+const lufthansa = {
+  airline: "Lufthansa",
+  iataCode: "LH",
+  bookings: [],
+  book(flightNum, name) {
+    console.log(
+      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+    );
+  },
 };
 
-const greeterHey = greet("Hey");
-greeterHey("Jonas");
-greet("Hello")("JOnas");
+lufthansa.book(239, "Jonas Schmedtmann");
+lufthansa.book(635, "John Wick");
 
-const greetArrow = (greeting) => (name) => console.log(`${greeting} ${name}`);
-greetArrow("Hello")("Trinity");
+const eurowings = {
+  airline: "Eurowings",
+  iataCode: "EW",
+  bookings: [],
+};
+
+const book = lufthansa.book;
+book.call(eurowings, 23, "Sarah Williams");
+console.log(eurowings);
+
+book.call(lufthansa, 239, "Mary Cooper");
+console.log(lufthansa);
+
+const swiss = {
+  airline: "Swiss Air Lines",
+  iataCode: "LX",
+  bookings: [],
+};
+
+book.call(swiss, 583, "Tony Stark");
+console.log(swiss);
