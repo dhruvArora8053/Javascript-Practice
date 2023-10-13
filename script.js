294e6364ee3516f4a5dfa11f903a684132a2a0f1
Dhruv Arora <dhruvarora.code@gmail.com>
2023-10-13 11:11:41 +0530

    implemented math and rounding

--- script.js
+++ script.js
@@ -4,31 +4,42 @@
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
-console.log(23 === 23.0);
-console.log(0.1 + 0.2);
-console.log(0.1 + 0.2 === 0.3);
+console.log(Math.sqrt(25));
+console.log(25 ** (1 / 2));
+console.log(8 ** (1 / 3));
 
-console.log(Number("23"));
-console.log(+"23");
+console.log(Math.max(5, 18, 23, 11, 2));
+console.log(Math.max(5, 18, "23", 11, 2));
+console.log(Math.max(5, 18, "23px", 11, 2));
 
-console.log(Number.parseInt("30px", 10));
-console.log(Number.parseInt("e23", 10));
-console.log(Number.parseInt("30px", 2));
+console.log(Math.min(5, 18, 23, 11, 2));
+console.log(Math.PI);
+console.log(Math.PI * Number.parseFloat("10px"));
 
-console.log(Number.parseFloat("2.5rem"));
-console.log(Number.parseInt("2.5rem"));
-console.log(Number.parseFloat("   2.5 rem  "));
+console.log(Math.trunc(Math.random() * 6 + 1));
 
-console.log(Number.isNaN(20));
-console.log(Number.isNaN("20"));
-console.log(Number.isNaN(+"20X"));
-console.log(Number.isNaN(23 / 0));
+const randomInt = (min, max) =>
+  Math.trunc(Math.random() * (max - min) + 1 + min);
 
-console.log(Number.isFinite(20));
-console.log(Number.isFinite("20"));
-console.log(Number.isFinite(+"20x"));
-console.log(Number.isFinite(23 / 0));
+console.log(randomInt(5, 20));
 
-console.log(Number.isInteger(23));
-console.log(Number.isInteger(23.0));
-console.log(Number.isInteger(23 / 0));
+console.log(Math.trunc(23.3));
+console.log(Math.round(23.3));
+console.log(Math.round(23.4));
+console.log(Math.round(23.5));
+console.log(Math.round(23.6));
+
+console.log(Math.ceil(23.3));
+console.log(Math.ceil(23.9));
+
+console.log(Math.floor(23.3));
+console.log(Math.floor(23.9));
+
+console.log(Math.floor("23.3"));
+console.log(Math.trunc(-23.3));
+console.log(Math.floor(-23.3));
+
+console.log((2.7).toFixed(0));
+console.log((2.7).toFixed(3));
+console.log((2.345).toFixed(2));
+console.log(+(2.345).toFixed);
