d1fda88f325cc4a6a88857fd689511fa95e3f19b
Dhruv Arora <dhruvarora.code@gmail.com>
2023-10-13 21:54:40 +0530

    implemented closures

--- script.js
+++ script.js
@@ -3,50 +3,16 @@
 //1. String method Practice
 //2. setters and getters
 
-const lufthansa = {
-  airline: "Lufthansa",
-  iataCode: "LH",
-  bookings: [],
-  book(flightNum, name) {
-    console.log(
-      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
-    );
-    this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
-  },
+const secureBooking = function () {
+  let passengerCount = 0;
+
+  return function () {
+    passengerCount++;
+    console.log(`${passengerCount} passengers`);
+  };
 };
 
-const eurowings = {
-  airline: "Eurowings",
-  iataCode: "EW",
-  bookings: [],
-};
-
-const swiss = {
-  airline: "Swiss Air LInes",
-  iataCode: "LX",
-  bookings: [],
-};
-
-const book = lufthansa.book;
-const bookEW = book.bind(eurowings);
-const bookLH = book.bind(lufthansa);
-const bookLX = book.bind(swiss);
-
-bookEW(23, "Steven Williams");
-console.log(eurowings);
-
-lufthansa.planes = 300;
-
-lufthansa.buyPlane = function () {
-  console.log(this);
-  this.planes++;
-  console.log(this.planes);
-};
-lufthansa.buyPlane();
-
-const addTax = (rate, value) => value + value * rate;
-console.log(addTax(0.1, 200));
-
-const addVAT = addTax.bind(null, 0.23);
-console.log(addVAT(100));
-console.log(addVAT(23));
+const booker = secureBooking();
+booker();
+booker();
+console.dir(booker);
