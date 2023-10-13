76dd05333f3756bde4a9e0223dcac6760d7f7bd3
Dhruv Arora <dhruvarora.code@gmail.com>
2023-10-13 10:35:40 +0530

    implemented sort method

--- script.js
+++ script.js
@@ -4,9 +4,27 @@
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
-//flat method:
-const arr = [[1, 2, 3], [4, 5, 6], 7, 8];
-console.log(arr.flat());
+const owners = ["Jonas", "Zach", "Adam", "Martha"];
+console.log(owners.sort());
+console.log(owners);
 
-const arrDeep = [[[1, 2], 3], [4, [5, 6]], 7, 8];
-console.log(arrDeep.flat(2));
+console.log(movements);
+// console.log(movements.sort());
+
+movements.sort((a, b) => {
+  if (a > b) return 1;
+  if (a < b) return -1;
+});
+console.log(movements);
+
+movements.sort((a, b) => {
+  if (a > b) return -1;
+  if (a < b) return 1;
+});
+console.log(movements);
+
+movements.sort((a, b) => a - b);
+console.log(movements);
+
+movements.sort((a, b) => b - a);
+console.log(movements);
