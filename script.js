88676fa0e125e020fbe2ec5aab0d99c318ca3372
Dhruv Arora <dhruvarora.code@gmail.com>
2023-10-13 10:26:36 +0530

    some and every methods implemented

--- script.js
+++ script.js
@@ -5,5 +5,12 @@
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 console.log(movements);
 
-const firstWithdrawal = movements.find((mov) => mov < 0);
-console.log(firstWithdrawal);
+console.log(movements.includes(-130));
+console.log(movements.some((mov) => mov === -130));
+
+const anyDeposits = movements.some((mov) => mov > 0);
+console.log(anyDeposits);
+console.log(movements.every((mov) => mov > 0));
+
+const deposit = (mov) => mov > 0;
+console.log(movements.some(deposit));
