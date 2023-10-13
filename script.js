e0c5de2bf5a605fd94adc8abacb01113048150f4
Dhruv Arora <dhruvarora.code@gmail.com>
2023-10-13 13:21:55 +0530

    implemented prototype

--- script.js
+++ script.js
@@ -2,25 +2,31 @@
 //Left:-
 //1. String method Practice
 
-const ingredients = ["spinach", "olive"];
+const Person0 = function (firstName, birthYear) {};
+new Person0("Jonas", 1991);
 
-const pizzaTimer = setTimeout(
-  (ing1, ing2) => console.log(`Here is your pizza with ${ing1} and ${ing2} 🍕`),
-  2000,
-  ...ingredients
-);
+const Person = function (firstName, birthYear) {
+  //   console.log(this);
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+};
 
-if (ingredients.includes("spinach")) clearTimeout(pizzaTimer);
+const jonas = new Person("Jonas", 1991);
+console.log(jonas);
+console.log(jonas instanceof Person);
 
-setInterval(() => {
-  console.log(new Date());
-}, 1000);
+Person.prototype.calcAge = function () {
+  console.log(2036 - this.birthYear);
+};
+console.log(Person.prototype);
 
-setInterval(() => {
-    let date = new Date();
-    let hour = date.getHours();
-    let minute = date.getMinutes();
-    let sec = date.getSeconds();
-    console.log(`${hour}:${minute}:${sec}`);
-  }, 1000);
-  
+jonas.calcAge();
+console.log(jonas);
+
+console.log(jonas.__proto__);
+console.log(jonas.__proto__ === Person.prototype);
+console.log(Person.prototype.isPrototypeOf(jonas));
+
+Person.prototype.species = "Homo Sapiens";
+console.log(jonas);
+console.log(jonas.species);
