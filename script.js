00b0eb080e9141fa874f0fb8383e72f158fce34c
Dhruv Arora <dhruvarora.code@gmail.com>
2023-10-13 10:28:24 +0530

    flat mehtod implemented

--- script.js
+++ script.js
@@ -3,14 +3,10 @@
 //1. String method Practice
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-console.log(movements);
 
-console.log(movements.includes(-130));
-console.log(movements.some((mov) => mov === -130));
+//flat method:
+const arr = [[1, 2, 3], [4, 5, 6], 7, 8];
+console.log(arr.flat());
 
-const anyDeposits = movements.some((mov) => mov > 0);
-console.log(anyDeposits);
-console.log(movements.every((mov) => mov > 0));
-
-const deposit = (mov) => mov > 0;
-console.log(movements.some(deposit));
+const arrDeep = [[[1, 2], 3], [4, [5, 6]], 7, 8];
+console.log(arrDeep.flat(2));
