b01f28b1e77ae2ee5eceba3798631f84e0beb45a
Dhruv Arora <dhruvarora.code@gmail.com>
2023-10-14 13:53:25 +0530

    new files added

--- script.js
+++ script.js
@@ -1,61 +1,9 @@
-"use strict";
-//Left:-
-//1. String method Practice
-//2. setters and getters
+'use strict';
 
-class Account {
-  locale = navigator.language;
-  #movements = [21, 44, 56];
-  #pin;
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
 
-  constructor(owner, currency, pin) {
-    this.owner = owner;
-    this.currency = currency;
-
-    this.#pin = pin;
-    console.log(`Thanks for opeing an account, ${owner}`);
-  }
-
-  getMovements() {
-    return this.#movements;
-  }
-
-  deposit(val) {
-    this.#movements.push(val);
-    return this;
-  }
-
-  withdraw(val) {
-    this.deposit(-val);
-    return this;
-  }
-
-  #approveLoan(val) {
-    return true;
-  }
-
-  requestLoan(val) {
-    if (this.#approveLoan(val)) {
-      this.deposit(val);
-      console.log(`Loan approved`);
-    }
-    return this;
-  }
-
-  static helper() {
-    console.log("Helper");
-  }
-}
-
-const acc1 = new Account("Jonas", "EUR", 1111);
-console.log(acc1);
-// console.log(acc1.getMovements());
-// console.log(acc1.#movements);
-console.log(acc1.getMovements());
-// console.log(acc1.#pin);
-// console.log(acc1.#approveLoan(100));
-
-Account.helper();
-
-acc1.deposit(300).deposit(500).withdraw(35).requestLoan(25000).withdraw(4000);
-console.log(acc1.getMovements());
+///////////////////////////////////////
+const request = new XMLHttpRequest();
+request.open('GET', 'https://restcountries.com/v3.1/name/portugal');
+request.send();
