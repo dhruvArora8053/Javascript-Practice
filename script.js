af52dcf4d66b277e3a750310748bc75b23ef9122
Dhruv Arora <dhruvarora.code@gmail.com>
2023-10-16 22:24:48 +0530

    promise removed

--- script.js
+++ script.js
@@ -49,14 +49,3 @@ const flights =
 // 🛑  Delayed Arrival HEL to FAO (12h05)
 //            Departure FAO to LIS (12h30)
 
-console.log('Test Start');
-setTimeout(() => console.log('0 sec timer'), 0);
-Promise.resolve('Resolved promise 1').then(res => console.log(res));
-Promise.resolve('Resolved promise 2').then(res => {
-  for (let i = 0; i < 1000; i++) {
-    console.log(res);
-  }
-});
-console.log('Test end');
-
-
