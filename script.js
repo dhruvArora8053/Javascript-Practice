e050ef0a0c61a305720c1226cf2f5a1b55569e45
Dhruv Arora <dhruvarora.code@gmail.com>
2023-11-04 19:49:58 +0530

    implemented sets

--- script.js
+++ script.js
@@ -121,3 +121,42 @@ console.log(staffUniqueArr);
 console.log(new Set(['Waiter', 'Chef', 'Manager', 'Chef', 'Waiter']).size);
 
 console.log(new Set('jonasschmedtmann'));
+
+const rest = new Map();
+rest.set('name', 'Classico Italino');
+rest.set(1, 'Firenze Italy');
+rest.set(2, 'Lisbon, Portugal');
+
+console.log(rest.set(2, 'Lisbon, Portugal'));
+
+rest
+  .set('categories', ['Italian', 'Pizzeria'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We are open')
+  .set(false, 'We are closed');
+console.log(rest);
+console.log(rest.get('name'));
+console.log(rest.get(true));
+console.log(rest.get('true'));
+console.log(rest.get(1));
+
+const time = 21;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+console.log(rest.has('categories'));
+rest.delete(2);
+console.log(rest);
+console.log(rest.size);
+// rest.clear();
+// console.log(rest);
+// rest.set([1, 2], 'Test');
+console.log(rest);
+
+// console.log(rest.get([1, 2]));
+const arr = [1, 2];
+rest.set(arr, 'Test');
+console.log(rest.get(arr));
+
+rest.set(document.querySelector('h1'), 'heading');
+console.log(rest);
