28d23316b75eed3e25d575e7602b36ce011ac018
Dhruv Arora <dhruvarora.code@gmail.com>
2023-11-04 20:07:55 +0530

    implmented strings

--- script.js
+++ script.js
@@ -190,3 +190,34 @@ console.log([...question]);
 console.log(question.entries());
 console.log(question.keys());
 console.log(question.values());
+
+const airline = 'TAP Air Portugal';
+const plane = 'A320';
+
+console.log(plane[0]);
+console.log(plane[1]);
+console.log(plane[2]);
+console.log(plane[3]);
+console.log('B737'[0]);
+console.log(airline.length);
+console.log('B737'.length);
+console.log(airline.indexOf('r'));
+console.log(airline.lastIndexOf('r'));
+console.log(airline.indexOf('Portugal'));
+console.log(airline.slice(4));
+console.log(airline.slice(4, 7));
+
+console.log(airline.slice(0, airline.indexOf(' ')));
+console.log(airline.slice(airline.lastIndexOf(' ') + 1));
+
+console.log(airline.slice(-2));
+console.log(airline.slice(1, -1));
+
+const checkMiddleSeat = function (seat) {
+  const lastWord = seat.slice(-1);
+  if (lastWord === 'A') console.log('fist seat');
+};
+
+console.log(new String('jonas'));
+console.log(typeof new String('jonas'));
+console.log(typeof new String('jonas').slice(1));
