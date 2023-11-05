5e74c41d84b205029cd85162c3f17f42eedc9351
Dhruv Arora <dhruvarora.code@gmail.com>
2023-11-05 16:04:45 +0530

    selecting elements

--- script.js
+++ script.js
@@ -1,309 +1,66 @@
 'use strict';
 
-const weekDays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+///////////////////////////////////////
+// Modal window
 
-const openingHours = {
-  [weekDays[3]]: {
-    open: 12,
-    close: 22,
-  },
-  fri: {
-    open: 11,
-    close: 23,
-  },
-  sat: {
-    open: 0,
-    close: 24,
-  },
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.btn--close-modal');
+const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
 };
 
-const restaurant = {
-  name: 'Classico Italiano',
-  location: 'Via Angelo Tavanti 23, Firenze Italy',
-  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-
-  openingHours,
-
-  order: function (starterIndex, mainIndex) {
-    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
-  },
-
-  orderDelivery: function ({
-    starterIndex = 1,
-    mainIndex = 0,
-    time = '20:00',
-    address,
-  }) {
-    console.log(
-      `Order recieved! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
-    );
-  },
-
-  orderPasta: function (ing1, ing2, ing3) {
-    console.log(`Here is your delicious pasta with ${ing1}, ${ing2}, ${ing3}`);
-  },
-
-  orderPizza: function (mainIngredient, ...otherIngredients) {
-    console.log(mainIngredient);
-    console.log(otherIngredients);
-  },
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
 };
 
-// console.log(restaurant.openingHours.mon);
-console.log(restaurant.openingHours?.mon?.open);
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', openModal);
 
-const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
-for (const day of days) {
-  const open = restaurant.openingHours[day]?.open ?? 'closed';
-  console.log(`On ${day}, we open at ${open}`);
-}
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
 
-console.log(restaurant.order?.(0, 1));
-console.log(restaurant.orderRisotto?.(0, 1));
-
-const users = [{ name: 'Jonas', email: 'hello@jona.io' }];
-console.log(users[0]?.name ?? 'user array empty');
-console.log(users[1]?.name ?? 'user array empty');
-
-const properties = Object.keys(openingHours);
-console.log(properties);
-
-let openStr = `We are open on ${properties.length} days: `;
-
-for (const day of Object.keys(openingHours)) {
-  console.log(day);
-}
-
-for (const day of properties) {
-  openStr += `${day}, `;
-}
-console.log(openStr);
-
-const values = Object.values(openingHours);
-console.log(values);
-
-const entries = Object.entries(openingHours);
-console.log(entries);
-
-for (const [key, { open, close }] of entries) {
-  console.log(`On ${key} we open at ${open} and close at ${close}`);
-}
-
-const ordersSet = new Set([
-  'Pasta',
-  'Pizza',
-  'Pizza',
-  'Risotto',
-  'Pasta',
-  'Pizza',
-]);
-console.log(ordersSet);
-
-console.log(new Set('Jonas'));
-console.log(ordersSet.size);
-console.log(ordersSet.has('Pizza'));
-ordersSet.add('Garlic Bread');
-ordersSet.add('Garlic Bread');
-ordersSet.delete('Risotto');
-console.log(ordersSet);
-
-for (const order of ordersSet) console.log(order);
-
-const staff = ['Waiter', 'Chef', 'Waiter', 'Manger', 'Chef', 'Waiter'];
-const staffUnique = new Set(staff);
-console.log(staffUnique);
-
-const staffUniqueArr = [...new Set(staff)];
-console.log(staffUniqueArr);
-
-console.log(new Set(['Waiter', 'Chef', 'Manager', 'Chef', 'Waiter']).size);
-
-console.log(new Set('jonasschmedtmann'));
-
-const rest = new Map();
-rest.set('name', 'Classico Italino');
-rest.set(1, 'Firenze Italy');
-rest.set(2, 'Lisbon, Portugal');
-
-console.log(rest.set(2, 'Lisbon, Portugal'));
-
-rest
-  .set('categories', ['Italian', 'Pizzeria'])
-  .set('open', 11)
-  .set('close', 23)
-  .set(true, 'We are open')
-  .set(false, 'We are closed');
-console.log(rest);
-console.log(rest.get('name'));
-console.log(rest.get(true));
-console.log(rest.get('true'));
-console.log(rest.get(1));
-
-const time = 21;
-console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
-
-console.log(rest.has('categories'));
-rest.delete(2);
-console.log(rest);
-console.log(rest.size);
-// rest.clear();
-// console.log(rest);
-// rest.set([1, 2], 'Test');
-console.log(rest);
-
-// console.log(rest.get([1, 2]));
-const arr = [1, 2];
-rest.set(arr, 'Test');
-console.log(rest.get(arr));
-
-rest.set(document.querySelector('h1'), 'heading');
-console.log(rest);
-
-const question = new Map([
-  ['question', 'what is the best programming language'],
-  [1, 'C'],
-  [2, 'Java'],
-  [3, 'Javascript'],
-  ['correct', 3],
-  [true, 'Correct'],
-  [false, 'Try Again'],
-]);
-console.log(question);
-
-console.log(Object.entries(openingHours));
-
-const hoursMap = new Map(Object.entries(openingHours));
-console.log(hoursMap);
-
-console.log(question.get('question'));
-for (const [key, value] of question) {
-  if (typeof key === 'number') {
-    console.log(`Answer ${key}: ${value}`);
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
   }
-}
+});
 
-const answer = 1;
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+console.log(document.querySelector('.header'));
 
-console.log([...question]);
-console.log(question.entries());
-console.log(question.keys());
-console.log(question.values());
+const allSections = document.querySelectorAll('.section');
+console.log(allSections);
+console.log(document.getElementById('section--1'));
 
-const airline = 'TAP Air Portugal';
-const plane = 'A320';
+const allButtons = document.getElementsByTagName('button');
+console.log(allButtons);
 
-console.log(plane[0]);
-console.log(plane[1]);
-console.log(plane[2]);
-console.log(plane[3]);
-console.log('B737'[0]);
-console.log(airline.length);
-console.log('B737'.length);
-console.log(airline.indexOf('r'));
-console.log(airline.lastIndexOf('r'));
-console.log(airline.indexOf('Portugal'));
-console.log(airline.slice(4));
-console.log(airline.slice(4, 7));
+console.log(document.getElementsByClassName('btn'));
 
-console.log(airline.slice(0, airline.indexOf(' ')));
-console.log(airline.slice(airline.lastIndexOf(' ') + 1));
+// .insertAdjacentHTML
 
-console.log(airline.slice(-2));
-console.log(airline.slice(1, -1));
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+message.innerHTML =
+  'We use cookies for improved functionality and analytics. <button class="btn btn-close--cookie">Got it!</button>';
 
-const checkMiddleSeat = function (seat) {
-  const lastWord = seat.slice(-1);
-  if (lastWord === 'A') console.log('fist seat');
-};
+const header = document.querySelector('.header');
+header.prepend(message);
+// header.append(message);
 
-console.log(new String('jonas'));
-console.log(typeof new String('jonas'));
-console.log(typeof new String('jonas').slice(1));
+// header.prepend(message.cloneNode(true));
 
-console.log(airline.toLowerCase());
-console.log(airline.toUpperCase());
-console.log('jonas'.toUpperCase());
+// header.before(message);
+// header.after(message);
 
-const passenger = 'jOnAs';
-const passengerLower = passenger.toLowerCase();
 
-const passengerCorrect =
-  passengerLower[0].toUpperCase() + passengerLower.slice(1);
-console.log(passengerCorrect);
 
-const email = 'hello@jonas.io';
-const loginEmail = '   Hello@Jonas.Io \n';
-
-const lowerEmail = loginEmail.toLowerCase();
-const trimmedEmail = lowerEmail.trim();
-console.log(trimmedEmail);
-
-const normalisedEmail = loginEmail.toLowerCase().trim();
-console.log(normalisedEmail);
-console.log(email == normalisedEmail);
-
-const priceGB = '288,97€';
-const priceUS = priceGB.replace('€', '$').replace(',', '.');
-console.log(priceUS);
-
-const announcement =
-  'All passengers come to boarding door 23.Boarding door 23!';
-
-console.log(announcement.replaceAll('door', 'gate'));
-
-const plane1 = 'Airbus A320neo';
-console.log(plane1.includes('A320'));
-console.log(plane1.includes('Boeing'));
-console.log(plane1.startsWith('Air'));
-console.log(plane1.startsWith('Airb'));
-
-if (plane.startsWith('Airbus') && plane.endsWith('neo')) {
-  console.log('Part of the new Airbus family');
-}
-
-console.log('a+very+nice+string'.split('+'));
-
-console.log('Jonas Schemedtmann'.split(' '));
-
-const [firstName, lastName] = 'Jonas Schemedtmann'.split(' ');
-
-const newName = ['Mr.', firstName, lastName.toUpperCase()].join(' ');
-console.log(newName);
-
-const capitalizeName = function (name) {
-  let upper = '';
-  for (const word of name.split(' ')) {
-    upper += word.replace(word[0], word[0].toUpperCase()) + ' ';
-  }
-  console.log(upper);
-};
-
-capitalizeName('jessica ann smith davis');
-capitalizeName('jonas schmedtmann');
-
-const message = 'Go to gate 23!';
-console.log(message.padStart(25, '+'));
-console.log('Jonas'.padStart(25, '+'));
-console.log(message.padStart(25, '+').padEnd(35, '+'));
-
-const maskCreditCard = function (number) {
-  const str = number + '';
-  const output = str.slice(-4).padStart(str.length, '*');
-  console.log(output);
-};
-
-maskCreditCard(73923749237493427);
-maskCreditCard('734907324972349783');
-
-const message2 = 'Bad weather... All Departues Delayed... ';
-console.log(message2.repeat(5));
-
-const planeInLine = function (n) {
-  console.log(`Thier are ${'✈️'.repeat(n)} in line`);
-};
-
-planeInLine(5);
-planeInLine(3);
-planeInLine(10);
+////////////////////////////////////////////////
+message.style.
