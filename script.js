c48fab15a10ae21c132ef6bfc39ebc7bf6093bbf
Dhruv Arora <dhruvarora.code@gmail.com>
2023-11-06 19:09:40 +0530

    implemented at method

--- script.js
+++ script.js
@@ -1,32 +1,12 @@
 'use strict';
 
-let arr = ['a', 'b', 'c', 'd', 'e'];
+const arr = [23, 11, 64];
 
-console.log(arr.slice(2));
-console.log(arr.slice(2, 4));
-console.log(arr.slice(-2));
-console.log(arr.slice(-1));
-console.log(arr.slice(1, -1));
-console.log(arr.slice(1, -2));
-console.log(arr.slice());
-console.log([...arr]);
-
-arr.splice(-1);
-console.log(arr);
-console.log(arr.splice(1, 2));
-console.log(arr);
-
-arr = ['a', 'b', 'c', 'd', 'e'];
-const arr2 = ['j', 'i', 'h', 'g', 'f'];
-console.log(arr2.reverse());
-console.log(arr2);
-
-const letters = arr.concat(arr2);
-console.log(letters);
-console.log([...arr, ...arr2]);
-
-console.log(letters.join('-'));
-
-let a = 10;
-console.log(a++);
-
+console.log(arr[0]);
+console.log(arr.at(0));
+console.log(arr[arr.length - 1]);
+console.log(arr.slice(-1)[0]);
+console.log(arr.at(-1));
+console.log(arr.at(-2));
+console.log('jonas'.at(0));
+console.log('jonas'.at(-1));
