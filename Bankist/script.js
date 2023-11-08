d224fc9bc74be1ac7e5e951cfe4a7cee239fc959
Dhruv Arora <dhruvarora.code@gmail.com>
2023-11-08 15:07:34 +0530

    implemented array methods

--- Bankist/script.js
+++ Bankist/script.js
@@ -1,36 +1,31 @@
 'use strict';
 
-const lufthansa = {
-  airline: 'Lufthansa',
-  iataCode: 'LH',
-  bookings: [],
-  book(flightNum, name) {
-    console.log(
-      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
-    );
-  },
-};
+console.log([1, 2, 3, 4, 5, 6, 7]);
+console.log(new Array(1, 2, 3, 4, 5, 6, 7));
 
-const eurwoings = {
-  airline: 'Eurwoings',
-  iataCode: 'EW',
-  bookings: [],
-};
+const x = new Array(7);
+console.log(x);
 
-const book = lufthansa.book;
-const bookEW = book.bind(eurwoings);
-const bookLH = book.bind(lufthansa);
+console.log(x.map(() => 5));
+x.map(() => 5);
 
-bookEW(23, 'Steven Williams');
-console.log(eurwoings);
+console.log(x.fill(1));
 
-const bookEW23 = book.bind(eurwoings, 23);
-bookEW23('Jonas Schmedtmann');
-bookEW23('Martha Cooper');
+const y = new Array(7);
+y.fill(1, 3, 5);
+console.log(y);
 
-const addTax = (rate, value) => value + value * rate;
-console.log(addTax(0.1, 200));
+const arr = [1, 2, 3, 4, 5, 6, 7, 8];
+arr.fill(23, 4, 7);
+console.log(arr);
 
-const addVAT = addTax.bind(null, 0.23);
-console.log(addVAT(100));
+const z = Array.from({ length: 7 }, () => 1);
+console.log(z);
 
+const a = Array.from({ length: 7 }, (cur, i) => i + 1);
+console.log(a);
+
+const diceRoll = Array.from({ length: 100 }, () =>
+  Math.trunc(Math.random() * 100 + 1)
+);
+console.log(diceRoll);
