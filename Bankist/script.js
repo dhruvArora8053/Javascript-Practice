0f97f70619cab25c2440a8a80de58812aaa13b70
Dhruv Arora <dhruvarora.code@gmail.com>
2023-11-08 15:33:29 +0530

    implemented dates

--- Bankist/script.js
+++ Bankist/script.js
@@ -1,24 +1,70 @@
 'use strict';
 
-console.log(Math.sqrt(25));
-console.log(25 ** (1 / 2));
-console.log(8 ** (1 / 3));
+// Data
+const account1 = {
+  owner: 'Jonas Schmedtmann',
+  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  interestRate: 1.2, // %
+  pin: 1111,
 
-console.log(Math.max(5, 18, 23, 11, 2));
-console.log(Math.max(5, 18, '23', 11, 2));
-console.log(Math.max(5, 18, '23px', 11, 2));
-console.log(Math.min(5, 18, 23, 11, 2));
-console.log(Math.PI);
-console.log(Math.PI * Number.parseFloat('10px'));
+  movementsDates: [
+    '2019-11-18T21:31:17.178Z',
+    '2019-12-23T07:42:02.383Z',
+    '2020-01-28T09:15:04.904Z',
+    '2020-04-01T10:17:24.185Z',
+    '2020-05-08T14:11:59.604Z',
+    '2020-05-27T17:01:17.194Z',
+    '2020-07-11T23:36:17.929Z',
+    '2020-07-12T10:51:36.790Z',
+  ],
+  currency: 'EUR',
+  locale: 'pt-PT', // de-DE
+};
 
-const randomInt = (min, max) =>
-  Math.floor(Math.random() * (max - min) + min + 1);
+const account2 = {
+  owner: 'Jessica Davis',
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
+  pin: 2222,
 
-console.log(randomInt(5, 20));
+  movementsDates: [
+    '2019-11-01T13:15:33.035Z',
+    '2019-11-30T09:48:16.867Z',
+    '2019-12-25T06:04:23.907Z',
+    '2020-01-25T14:18:46.235Z',
+    '2020-02-05T16:33:06.386Z',
+    '2020-04-10T14:43:26.374Z',
+    '2020-06-25T18:49:59.371Z',
+    '2020-07-26T12:01:20.894Z',
+  ],
+  currency: 'USD',
+  locale: 'en-US',
+};
 
-console.log(Math.trunc(23.3));
-console.log(Math.round(23.3));
+const now = new Date();
+console.log(now);
 
-console.log((2.7).toFixed(0));
-console.log((2.7).toFixed(3));
-console.log(+(2.345).toFixed(2));
+console.log(new Date('Apr 07 2023 22:14:37'));
+console.log(new Date('December 24, 2015'));
+
+console.log(account1.movementsDates[0]);
+
+console.log(new Date(2037, 10, 19, 15, 23, 5));
+console.log(new Date(2037, 10, 31));
+console.log(new Date(2037, 10, 33));
+console.log(new Date(0));
+console.log(new Date(3 * 24 * 60 * 60 * 1000));
+const future = new Date(2037, 10, 19, 15, 23, 44);
+console.log(future);
+console.log(future.getFullYear(0));
+console.log(future.getDate());
+console.log(future.getMonth());
+console.log(future.getDay());
+console.log(future.getHours());
+console.log(future.getMinutes());
+console.log(future.getSeconds());
+console.log(future.getTime());
+console.log(new Date(2142237224000));
+console.log(Date.now());
+future.setFullYear(2040);
+console.log(future);
