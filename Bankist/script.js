b296b3c7425d761c4db358234919587877bf4a00
Dhruv Arora <dhruvarora.code@gmail.com>
2023-11-08 13:23:13 +0530

    implemented regular function vs arrow function

--- Bankist/script.js
+++ Bankist/script.js
@@ -1,36 +1,66 @@
 'use strict';
 
-console.log(this);
-
-const calcAge = function (birthYear) {
-  console.log(2037 - birthYear);
-  console.log(this);
-};
-
-calcAge(1991);
-
-const calcAgeArrow = birthYear => {
-  console.log(2037 - birthYear);
-  console.log(this);
-};
-calcAgeArrow(1980);
-
 const jonas = {
+  firstName: 'Jonas',
   year: 1991,
   calcAge: function () {
     console.log(this);
     console.log(2037 - this.year);
   },
+
+  greet: () => console.log(`Hey ${this.firstName}`),
 };
 
-jonas.calcAge();
+jonas.greet();
 
-const matilda = {
-  year: 2017,
+var firstName = 'Matilda';
+
+const jonas1 = {
+  firstName: 'Jonas',
+  year: 1991,
+  calcAge: function () {
+    console.log(this);
+    console.log(2037 - this.year);
+  },
+
+  greet: () => {
+    console.log(this);
+    console.log(this.firstName);
+    console.log(`Hey ${this.firstName}`);
+  },
 };
-matilda.calcAge = jonas.calcAge;
-console.log(matilda);
 
-const f = jonas.calcAge;
-console.log(f);
-f();
+jonas1.greet();
+
+const jonas2 = {
+  year: 1991,
+  calcAge: function () {
+    console.log(this);
+    console.log(2037 - this.year);
+
+    const self = this;
+    const isMillenial = function () {
+      // console.log(this);
+      console.log(self);
+      console.log(self.year >= 1981 && self.year <= 1996);
+    };
+
+    const isMillenial1 = () => {
+      console.log();
+    };
+    isMillenial();
+  },
+};
+
+const addExpr = function (a, b) {
+  console.log(arguments);
+  return a + b;
+};
+
+addExpr(2, 5);
+
+const addArrow = (a, b) => {
+  console.log(arguments);
+  return a + b;
+};
+addArrow(2, 5, 8);
