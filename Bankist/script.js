077256f283c39eb7eec2ac80887a660941d71c61
Dhruv Arora <dhruvarora.code@gmail.com>
2023-11-08 15:22:42 +0530

    math rounding implemented

--- Bankist/script.js
+++ Bankist/script.js
@@ -1,30 +1,24 @@
 'use strict';
 
-console.log(23 === 23.0);
-console.log(0.1 + 0.2);
-console.log(0.1 + 0.2 === 0.3);
+console.log(Math.sqrt(25));
+console.log(25 ** (1 / 2));
+console.log(8 ** (1 / 3));
 
-console.log(Number('23'));
+console.log(Math.max(5, 18, 23, 11, 2));
+console.log(Math.max(5, 18, '23', 11, 2));
+console.log(Math.max(5, 18, '23px', 11, 2));
+console.log(Math.min(5, 18, 23, 11, 2));
+console.log(Math.PI);
+console.log(Math.PI * Number.parseFloat('10px'));
 
-console.log(Number.parseInt('30px', 10));
-console.log(Number.parseInt('e23', 10));
-console.log(Number.parseInt('30px', 2));
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min) + min + 1);
 
-console.log(Number.parseFloat('2.5rem'));
-console.log(Number.parseInt('2.5rem'));
-console.log(Number.parseFloat('   2.5rem  '));
-console.log(parseFloat('    2.5rem  '));
+console.log(randomInt(5, 20));
 
-console.log(Number.isNaN(20));
-console.log(Number.isNaN('20'));
-console.log(Number.isNaN(+'20x'));
-console.log(Number.isNaN(23 / 0));
+console.log(Math.trunc(23.3));
+console.log(Math.round(23.3));
 
-console.log(Number.isFinite(20));
-console.log(Number.isFinite('20'));
-console.log(Number.isFinite(+'20x'));
-console.log(Number.isFinite(23 / 0));
-
-console.log(Number.isInteger(23));
-console.log(Number.isInteger(23.0));
-console.log(Number.isInteger(23 / 0));
+console.log((2.7).toFixed(0));
+console.log((2.7).toFixed(3));
+console.log(+(2.345).toFixed(2));
