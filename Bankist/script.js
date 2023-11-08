1f834fe0e6a5b769861e274d2d37ccde0e285f10
Dhruv Arora <dhruvarora.code@gmail.com>
2023-11-08 16:17:36 +0530

    implemented internationalization

--- Bankist/script.js
+++ Bankist/script.js
@@ -1,53 +1,19 @@
 'use strict';
 
-// Data
-const account1 = {
-  owner: 'Jonas Schmedtmann',
-  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
-  interestRate: 1.2, // %
-  pin: 1111,
+const num = 3884764.23;
+console.log('US:     ', new Intl.NumberFormat('en-US').format(num));
+console.log('Germany:', new Intl.NumberFormat('de-DE').format(num));
+console.log('Syria:  ', new Intl.NumberFormat('ar-SY').format(num));
+console.log(
+  navigator.language,
+  new Intl.NumberFormat(navigator.language).format(num)
+);
 
-  movementsDates: [
-    '2019-11-18T21:31:17.178Z',
-    '2019-12-23T07:42:02.383Z',
-    '2020-01-28T09:15:04.904Z',
-    '2020-04-01T10:17:24.185Z',
-    '2020-05-08T14:11:59.604Z',
-    '2020-05-27T17:01:17.194Z',
-    '2020-07-11T23:36:17.929Z',
-    '2020-07-12T10:51:36.790Z',
-  ],
+const options = {
+  style: 'currency',
+  unit: 'mile-per-hour',
   currency: 'EUR',
-  locale: 'pt-PT', // de-DE
 };
-
-const account2 = {
-  owner: 'Jessica Davis',
-  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
-  interestRate: 1.5,
-  pin: 2222,
-
-  movementsDates: [
-    '2019-11-01T13:15:33.035Z',
-    '2019-11-30T09:48:16.867Z',
-    '2019-12-25T06:04:23.907Z',
-    '2020-01-25T14:18:46.235Z',
-    '2020-02-05T16:33:06.386Z',
-    '2020-04-10T14:43:26.374Z',
-    '2020-06-25T18:49:59.371Z',
-    '2020-07-26T12:01:20.894Z',
-  ],
-  currency: 'USD',
-  locale: 'en-US',
-};
-
-const future = new Date(2037, 10, 19, 15, 23);
-console.log(future);
-console.log(+future);
-
-const daysPassed = (date1, date2) =>
-  Math.abs(date2 - date1) / (1000 * 60 * 60 * 24);
-
-const days1 = daysPassed(new Date(2037, 3, 14), new Date(2037, 3, 24));
-console.log(days1);
-
+console.log('US:   ', new Intl.NumberFormat('en-US', options).format(num));
+console.log('Germany:', new Intl.NumberFormat('de-DE', options).format(num));
+console.log('Syria:', new Intl.NumberFormat('ar-SY', options).format(num));
