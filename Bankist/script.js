ff0bd78b6349930befb4bd2ff647803072580479
Dhruv Arora <dhruvarora.code@gmail.com>
2023-11-08 16:22:02 +0530

    implemented timer

--- Bankist/script.js
+++ Bankist/script.js
@@ -1,19 +1,11 @@
 'use strict';
 
-const num = 3884764.23;
-console.log('US:     ', new Intl.NumberFormat('en-US').format(num));
-console.log('Germany:', new Intl.NumberFormat('de-DE').format(num));
-console.log('Syria:  ', new Intl.NumberFormat('ar-SY').format(num));
-console.log(
-  navigator.language,
-  new Intl.NumberFormat(navigator.language).format(num)
+const ingredients = ['spinach', 'olive'];
+
+const pizzaTimer = setTimeout(
+  (ing1, ing2) => console.log(`Here is your pizza with ${ing1} and ${ing2} 🍕`),
+  2000,
+  ...ingredients
 );
 
-const options = {
-  style: 'currency',
-  unit: 'mile-per-hour',
-  currency: 'EUR',
-};
-console.log('US:   ', new Intl.NumberFormat('en-US', options).format(num));
-console.log('Germany:', new Intl.NumberFormat('de-DE', options).format(num));
-console.log('Syria:', new Intl.NumberFormat('ar-SY', options).format(num));
+if (ingredients.includes('spinach')) clearTimeout(pizzaTimer);
