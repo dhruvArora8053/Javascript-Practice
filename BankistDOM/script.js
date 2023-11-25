ba14650947d7db16a1bf895f0c2e36d8e4d9b1c7
Dhruv Arora <dhruvarora.code@gmail.com>
2023-11-25 14:04:20 +0530

    implementing dom manipulation

--- BankistDOM/script.js
+++ BankistDOM/script.js
@@ -29,3 +29,82 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+/////////////////////////////////////////////////
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+console.log(document.querySelector('.header'));
+
+const allSections = document.querySelectorAll('.section');
+console.log(allSections);
+console.log(document.getElementById('section--1'));
+const allButtons = document.getElementsByTagName('button');
+console.log(allButtons);
+
+console.log(document.getElementsByClassName('btn'));
+
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+message.innerHTML =
+  'We use cookies for improved functionality and analytics. <button class="btn btn--close--cookie">Got it!</button>';
+
+const header = document.querySelector('.header');
+// header.prepend(message);
+header.append(message);
+// header.prepend(message.cloneNode(true));
+
+// header.before(message);
+// header.after(message)
+
+document
+  .querySelector('.btn--close--cookie')
+  .addEventListener('click', function () {
+    message.remove();
+  });
+
+message.style.backgroundColor = '#37383d';
+message.style.width = '120%';
+
+console.log(message.style.height);
+console.log(message.style.backgroundColor);
+console.log(getComputedStyle(message).color);
+console.log(getComputedStyle(message).height);
+
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height) + 30 + 'px';
+console.log(message.style.height);
+
+document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+const logo = document.querySelector('.nav__logo');
+console.log(logo.alt);
+console.log(logo.src);
+console.log(logo.getAttribute('src'));
+console.log(logo.className);
+
+console.log(logo.designer);
+console.log(logo.getAttribute('designer'));
+
+logo.alt = 'Beautiful minimalist logo';
+console.log(logo.alt);
+
+logo.setAttribute('company', 'Bankist');
+console.log(logo.getAttribute('company'));
+
+const link = document.querySelector('.nav__link--btn');
+console.log(link.href);
+console.log(link.getAttribute('href'));
+console.log(logo.dataset.versionNumber);
+
+logo.classList.add('c', 'j');
+logo.classList.remove('c', 'j');
+logo.classList.toggle('c', 'j');
+logo.classList.contains('c', 'j');
+
+logo.className = 'jonas';
+
+const h1 = document.querySelector('h1');
+h1.addEventListener('mouseenter', function (e) {
+  console.log('Great you are reading the heading');
+});
