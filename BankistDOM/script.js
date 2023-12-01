9b2548f9d0be7578dcb9011fa1e7c3fd3c0593fd
Dhruv Arora <dhruvarora.code@gmail.com>
2023-12-01 23:02:13 +0530

    implemented smooth scrolling old way

--- BankistDOM/script.js
+++ BankistDOM/script.js
@@ -9,3 +9,11 @@ btnScrollTo.addEventListener('click', function (e) {
     behavior: 'smooth',
   });
 });
+
+document.querySelectorAll('.nav__link').forEach(el => {
+  el.addEventListener('click', function (e) {
+    e.preventDefault();
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({behavior: 'smooth'})
+  });
+});
